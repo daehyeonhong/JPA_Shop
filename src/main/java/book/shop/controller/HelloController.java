433b1c7d2faf8cef2a9a442a0fca6fe9ba255925
DaeHyeon <fholng@outlook.com>
2022-02-22 00:29:06 +0900

    [JPA] Core Git Init!

---
+++ src/main/java/book/shop/controller/HelloController.java
@@ -0,0 +1,14 @@
+package book.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping(value = "hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "World!");
+        return "hello";
+    }
+}
---
+++ src/test/java/book/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package book.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
