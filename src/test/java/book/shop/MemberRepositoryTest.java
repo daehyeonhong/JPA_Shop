879487cb9e91bc5d8bf08ee6f8665e73bed42045
DaeHyeon <fholng@outlook.com>
2022-02-23 23:46:26 +0900

    [JPA] H2 -> MySQL

--- src/test/java/book/shop/MemberRepositoryTest.java
+++ src/test/java/book/shop/MemberRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,9 +18,9 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional
-    @Rollback(value = false)
+//    @Rollback(value = false)
     @DisplayName(value = "멤버 찾기")
-    public void testMember() {
+    void testMember() {
         //given
         final Member member = new Member();
         member.setUsername("memberA");
