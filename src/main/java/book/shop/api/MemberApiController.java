899165f003c5f1cae212a816d5fd1185bbaf536b
HonGDae <fholng@outlook.com>
2022-03-06 15:15:50 +0900

    [Member] 회원 조회 API

--- src/main/java/book/shop/api/MemberApiController.java
+++ src/main/java/book/shop/api/MemberApiController.java
@@ -6,6 +6,7 @@ import book.shop.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,11 +14,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
+
+    @GetMapping(value = "/api/v1/members")
+    public List<Member> membersV1() {
+        return this.memberService.findMembers();
+    }
+
+    @GetMapping(value = "/api/v2/members")
+    public Result<List<MemberDto>> membersV2() {
+        final List<MemberDto> memberDtos = this.memberService.findMembers().stream().map(member -> new MemberDto(member.getName())).collect(Collectors.toList());
+        return new Result<>(memberDtos.size(), memberDtos);
+    }
 
     @PostMapping(value = "/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid final Member member) {
@@ -35,11 +49,25 @@ public class MemberApiController {
     }
 
     @PutMapping(value = "/api/v2/members/{id}")
-    public UpdateMemberResponse updateMemberV2(@PathVariable final Long id,
-                                               @RequestBody @Valid UpdateMemberRequest memberRequest) {
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable final Long id,
+            @RequestBody @Valid UpdateMemberRequest memberRequest) {
         this.memberService.update(id, memberRequest.getName(), memberRequest.getAddress());
         final Member member = this.memberService.findOne(id);
         return new UpdateMemberResponse(member.getId(), member.getName(), member.getAddress());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
     }
 
     @Data
