d7e207c61283c609c16302627051364133a6f5be
hongdae <fholng@outlook.com>
2022-03-15 00:01:52 +0900

    [Order] 주문 조회 DTO

--- src/main/java/book/shop/api/OrderApiController.java
+++ src/main/java/book/shop/api/OrderApiController.java
@@ -1,12 +1,19 @@
 package book.shop.api;
 
+import book.shop.domain.Address;
 import book.shop.domain.Order;
 import book.shop.domain.OrderItem;
+import book.shop.enumerate.OrderStatus;
 import book.shop.repository.OrderRepository;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +30,49 @@ public class OrderApiController {
             orderItems.stream().forEach(orderItem -> orderItem.getItem().getName());
         }
         return orders;
+    }
+
+    @GetMapping(value = "/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        return this.orderRepository.findAll().stream()
+                .map(OrderDto::new)
+                .collect(toList());
+    }
+
+    @Getter
+    @Setter
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(final Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+            this.orderItems = order.getOrderItems().stream()
+                    .map(OrderItemDto::new).collect(toList());
+        }
+    }
+
+    @Getter
+    @Setter
+    static class OrderItemDto {
+        private String itemName;
+        private BigDecimal orderPrice;
+        private int count;
+        private BigDecimal totalPrice;
+
+        public OrderItemDto(final OrderItem orderItem) {
+            this.itemName = orderItem.getItem().getName();
+            this.orderPrice = orderItem.getOrderPrice();
+            this.count = orderItem.getCount();
+            this.totalPrice = orderItem.getTotalPrice();
+        }
     }
 }
